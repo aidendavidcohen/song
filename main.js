6c38ca5268fb8328fe047c9237c2726002ed787c
PolyEd <andrew@poly-ed.com>
2024-07-05 11:50:23 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,88 @@
+let rectx = 940;
+let recty = 362;
+let width = 1880;
+let height = 725;
+let speedy = 1;
+let speedx = 1;
+let mincolor;
+let r;
+let g;
+let b;
+let rstep = 1;
+let bstep = 1;
+let gstep = 1;
+let sw = 50;
+let START = false;
+function sliderChangeSW(val) {
+sw = parseInt(val)
+}
+function sliderChangeSY(valy) {
+speedy = parseInt(valy)
+}
+function sliderChangeSX(valx) {
+speedx = parseInt(valx)
+}
+function sliderChangeR(valr) {
+r = parseInt(valr);
+}
+function sliderChangeG(valg) {
+g = parseInt(valg);
+}
+function sliderChangeB(valb) {
+b = parseInt(valb);
+}
+
+function buttonStart(){
+START = true;
+}
+function buttonStop(){
+START = false;
+}
+console.log("i got here")
+function setup(){
+mincolor = random(70,155);
+createCanvas(width,height);
+frameRate(520);
+background(0);
+r = random(1,256);
+g = random(1,256);
+b = random(1,256);
+}
+function draw(){
+if(START){
+//rect(x, y, width, height)
+fill(r,g,b);
+strokeWeight(0);
+rect(rectx, recty, sw, sw);
+if (rectx>=width-sw||rectx<=0){
+speedx = -speedx
+}
+if (recty>=height-sw||recty<=0){
+speedy = -speedy
+}
+rectx +=speedx;
+recty +=speedy;
+if (r>=255){
+rstep = -1;
+}
+if (g>=255){
+gstep = -1;
+}
+if (b>=255){
+bstep = -1;
+}
+if (r<=mincolor){
+rstep = 1;
+}
+if (g<=mincolor){
+gstep = 1;
+}
+if (b<=mincolor){
+bstep = 1;
+}
+r = (r+rstep);
+g = (g+gstep);
+b = (b+bstep);
+console.log(r,g,b)
+}
+}
